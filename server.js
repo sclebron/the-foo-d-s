79757b88ef1edc9a7e8c9a5eb747e2163c883482
SLeBron <sophieclebron@gmail.com>
2023-02-24 19:06:06 -0700

    updated server.js;

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+
 const sess = {
   secret: "Super secret secret",
   cookie: {},
@@ -37,3 +39,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
+
