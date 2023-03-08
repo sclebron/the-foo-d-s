f069ce88092ddeb46494af42bab928896440f4bf
SLeBron <sophieclebron@gmail.com>
2023-03-08 16:37:56 -0700

    updated database

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
+const sequelize = new Sequelize(
+      // process.env.DB_NAME,
+      // process.env.DB_USER,
+      // process.env.DB_PASSWORD,
+      'foo_ds_db',
+      'root',
+      'password',
       {
         host: "localhost",
         dialect: "mysql",
         port: 3306,
       }
-    );
+);
 
 module.exports = sequelize;
