ab5f6e8976b0e70b7e9a732735f00ccfae0baf90
SLeBron <sophieclebron@gmail.com>
2023-05-09 22:35:28 -0600

    restructured connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,24 @@
 const Sequelize = require('sequelize');
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      // 'foo_ds_db',
-      // 'root',
-      // 'password',
-      {
-        host: "127.0.0.1",
-        dialect: "mysql",
-        port: 3306,
-      }
-);
+const sequelize = new Sequelize({
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+  dialect: "mysql",
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+});
+
+// const sequelize = new Sequelize(
+//       process.env.DB_NAME,
+//       process.env.DB_USER,
+//       process.env.DB_PASSWORD,
+//       {
+//         host: "127.0.0.1",
+//         dialect: "mysql",
+//         port: 3306,
+//       }
+// );
 
 module.exports = sequelize;
